a80c02d31cd8d63394fd47f6dcb0451b98d9ca59
abeljlim <subrosian@hotmail.com>
2023-04-11 17:56:52 -0700

    Using an array and recursion for nested callbacks is my plan; this is the first part of that

---
+++ survey.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const questions = [
+  `What's your name? Nicknames are also acceptable :) `,
+  `What's an activity you like doing? `,
+  `What do you listen to while doing that? `,
+  `Which meal is your favourite (eg: dinner, brunch, etc.) `,
+  `What's your favourite thing to eat for that meal?`, 
+  `Which sport is your absolute favourite? `, 
+  `What is your superpower? In a few words, tell us what you are amazing at! `
+];
+
+rl.question('What\'s your name? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+
+  rl.question('What\'s an activity you like doing? ', (answer) => {
+    // TODO: Log the answer in a database
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+
+    rl.close();
+  });
+});
+
+/*
+Profile example:
+Devani loves listening to Ludovico Einaudi while coding, devouring Yak Momos for brunch, prefers Tennis over any other sport, and is amazing at dropping mad puns at inopportune times.
+*/
+
+// current version of the code, but will use the one for node v12.7
+// // basic use of the node:readline module
+// const readline = require('node:readline');
+// const { stdin: input, stdout: output } = require('node:process');
+
+// const rl = readline.createInterface({ input, output });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//   rl.close();
+// });
